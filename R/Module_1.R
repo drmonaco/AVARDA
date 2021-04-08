f8f71a8f6d88a5bbc459b8ea94269508366d6071
drmonaco <dmonaco1@jhmi.edu>
2021-04-08 11:00:45 -0400

    Uploading new module 1

--- R/Module_1.R
+++ R/Module_1.R
@@ -1,8 +1,14 @@
-virus_p = fread("Phageome/total_probs_phageome_virus.csv")
-case = fread("Phageome/phipseq_0140_PhageomeLa_001_Hits_foldchange.tsv")[,1:2]
+Module_1 = function(case_data,blast,total_prob,dict){
 
-Module_1 = function(case_data,blast,total_prob){
-case_data.R = case_data
+case_data.R = case_data %>% filter(.[[2]] > 5) %>% left_join(blast) %>% select(-2)
 
+case.binary = binary(case_data.R,80)
 
+csums_index = which(case.binary[-1] %>% colSums()  > 5) +1
+
+case.binary.sub = case.binary %>% select(c(1,csums_index))
+
+case.binary.sub.2 = (case.binary.sub [-1]%>% colSums()) %>% as.data.frame()
+
+return(case.binary.sub)
 }
---
+++ R/binary.R
@@ -0,0 +1,6 @@
+binary = function(matrix,threshold){
+  g2 <- matrix %>% as.data.frame()%>% mutate_if(is.numeric, (funs(as.integer(./threshold)))) %>% as.data.frame()
+  return(g2)
+  }
+
+
