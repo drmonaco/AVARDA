2c4f5ea66f04c48eded340549948d46a66d21ed2
drmonaco <dmonaco1@jhmi.edu>
2021-04-08 13:14:58 -0400

    Test

--- R/Module_1.R
+++ R/Module_1.R
@@ -8,8 +8,10 @@ csums_index = which(case.binary[-1] %>% colSums()  > 5) + 1 # get just viruses t
 
 case.binary.sub.evidence = case.binary %>% select(c(1,csums_index)) # subset evidence peptides above index - binary matrix of evidence peptides by alignnment
 
-case.binary.sub.2 = (case.binary.sub [-1]%>% colSums()) %>% as.data.frame() %>% rename(peps = ".") %>% mutate(all_peptides = dim(case.binary.sub.evidence)[1]) %>%
-  rownames_to_column() %>% left_join(total_prob) %>% mutate(pVal = mapply(bt, case.binary.sub.2$peps, case.binary.sub.2$all_peptides,case.binary.sub.2$V1)) %>%
+case.binary.sub.2 = (case.binary.sub.evidence [-1]%>% colSums()) %>% as.data.frame() %>% rename(peps = ".") %>% mutate(all_peptides = dim(case.binary.sub.evidence)[1]) %>%
+  rownames_to_column() %>% left_join(total_prob)
+
+  case.binary.sub.2 = case.binary.sub.2 %>% mutate(pVal = mapply(bt, case.binary.sub.2$peps, case.binary.sub.2$all_peptides,case.binary.sub.2$V1)) %>%
   arrange(pVal)
 
 case.binary.sub.all = case_data.R %>% filter(.[[1]] %in% case.binary.sub.evidence[[1]]) # unbinaried data for all viruses
