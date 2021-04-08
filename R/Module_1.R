41b5f5a658a97ccebdae5f7e422492df9b3c3cf3
drmonaco <dmonaco1@jhmi.edu>
2021-04-08 11:27:53 -0400

    Improved module 1 - produces three outputs

--- R/Module_1.R
+++ R/Module_1.R
@@ -1,14 +1,18 @@
 Module_1 = function(case_data,blast,total_prob,dict){
 
-case_data.R = case_data %>% filter(.[[2]] > 5) %>% left_join(blast) %>% select(-2)
+case_data.R = case_data %>% filter(.[[2]] > 5) %>% left_join(blast) %>% select(-2) # threshold hits matrix and create blast matrix
 
-case.binary = binary(case_data.R,80)
+case.binary = binary(case_data.R,80) # create binary matrix of evidence alignments
 
-csums_index = which(case.binary[-1] %>% colSums()  > 5) +1
+csums_index = which(case.binary[-1] %>% colSums()  > 5) + 1 # get just viruses that are enriched
 
-case.binary.sub = case.binary %>% select(c(1,csums_index))
+case.binary.sub.evidence = case.binary %>% select(c(1,csums_index)) # subset evidence peptides above index - binary matrix of evidence peptides by alignnment
 
-case.binary.sub.2 = (case.binary.sub [-1]%>% colSums()) %>% as.data.frame()
+case.binary.sub.2 = (case.binary.sub [-1]%>% colSums()) %>% as.data.frame() # summary of number of evidence peptides per virus
 
-return(case.binary.sub)
+case.binary.sub.all = case_data.R %>% filter(.[[1]] %in% case.binary.sub.evidence[[1]]) # unbinaried data for all viruses
+
+output = list(case.binary.sub.evidence,case.binary.sub.2,case.binary.sub.all)
+
+return(output)
 }
