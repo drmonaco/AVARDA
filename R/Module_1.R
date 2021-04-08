3989ddf19e0f2bcdf1602a05b3904e52fc117155
drmonaco <dmonaco1@jhmi.edu>
2021-04-08 12:55:00 -0400

    Module 1 - created 3 list upload

--- R/Module_1.R
+++ R/Module_1.R
@@ -8,9 +8,9 @@ csums_index = which(case.binary[-1] %>% colSums()  > 5) + 1 # get just viruses t
 
 case.binary.sub.evidence = case.binary %>% select(c(1,csums_index)) # subset evidence peptides above index - binary matrix of evidence peptides by alignnment
 
-case.binary.sub.2 = (case.binary.sub [-1]%>% colSums()) %>% as.data.frame() %>% mutate(all_peptides = dim(case.binary.sub.evidence)[1]) %>%
-  rownames_to_column() %>% left_join(total_prob)
-case.binary.sub.2$pVal <- mapply(bt, pad_sum$PAD2_ind, pad_sum$test)
+case.binary.sub.2 = (case.binary.sub [-1]%>% colSums()) %>% as.data.frame() %>% rename(peps = ".") %>% mutate(all_peptides = dim(case.binary.sub.evidence)[1]) %>%
+  rownames_to_column() %>% left_join(total_prob) %>% mutate(pVal = mapply(bt, case.binary.sub.2$peps, case.binary.sub.2$all_peptides,case.binary.sub.2$V1)) %>%
+  arrange(pVal)
 
 case.binary.sub.all = case_data.R %>% filter(.[[1]] %in% case.binary.sub.evidence[[1]]) # unbinaried data for all viruses
 
@@ -20,4 +20,3 @@ return(output)
 }
 
 
-case.binary.sub.2 = case.binary.sub.2 %>% (pVal = binom.test(peps,all_peptides,V1))
--- R/bt.R
+++ R/bt.R
@@ -1,2 +1,2 @@
-bt <- function(a, b, p = 0.5) {binom.test(a, b, 0.5, alternative=
-                                            c("two.sided"), conf.level = 0.95)$p.value}
+bt <- function(a, b, p = 0.5) {binom.test(a, b, p, alternative=
+                                            c("greater"), conf.level = 0.95)$p.value}
