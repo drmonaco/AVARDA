9025d29128121637a1931b5fee2150caffb0f9a4
drmonaco <dmonaco1@jhmi.edu>
2021-04-13 14:58:46 -0400

    V.0.8

---
+++ R/compiler.R
@@ -0,0 +1,20 @@
+#' @export
+
+compiler = function(x,blast,total_prob,dict,threshold = 5){
+
+  all = filter_avarda(edge = dict,vertex = x %>% select(1))
+  df = data.frame(matrix(ncol = 2,nrow = dim(x)[2]))
+  colnames(df) = c("virus","filtered")
+  for(R in 2:dim(x)[2]){
+    x.R = x %>% binary(threshold = 80) %>%  filter(.[[R]] > 0) %>% select(1)
+    df[R,1] = colnames(x)[R]
+    df[R,2] = filter_avarda(edge = dict,vertex = x.R)
+  }
+  df$all_f = all
+  df = df %>% left_join(total %>% rename(virus = "rowname"),by = "virus") %>% filter(!is.na(virus))
+
+  df = df %>% mutate(pVal_f = mapply(bt, df$filtered, df$all_f,df$V1)) %>% arrange(pVal_f) %>% cbind(dim(x)[1]) %>% cbind(dim(x.R)[1])
+  colnames(df) = c("virus","filter_evidence","all_filtered","null_prob","pVal","all_peptides","evidence_peptides")
+  return(df)
+}
+
