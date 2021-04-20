1ccd9b1781f332d334b1bc0c6e84caa804d90ec3
drmonaco <dmonaco1@jhmi.edu>
2021-04-20 16:57:34 -0400

    Module 2 speed up binary outside of for loop

--- R/Module_2.R
+++ R/Module_2.R
@@ -4,9 +4,10 @@ Module_2 = function(x,dict,total_prob){
   all = filter_avarda(edge = dict,vertex = x %>% select(1))
   df = data.frame(matrix(ncol = 2,nrow = dim(x)[2]))
   colnames(df) = c("virus","filtered")
+  x = x %>% binary(threshold = 80)
   for(R in 2:dim(x)[2]){
-  if(dim(x %>% filter(.[[R]] > 80))[1]>=3){
-  x.R = x %>% binary(threshold = 80) %>%  filter(.[[R]] > 0) %>% select(1)
+  if(dim(x %>% filter(.[[R]] > 0))[1]>=3){
+  x.R = x %>%  filter(.[[R]] > 0) %>% select(1) %>% unlist()
   df[R,1] = colnames(x)[R]
   df[R,2] = filter_avarda(edge = dict,vertex = x.R)
   }
@@ -17,5 +18,3 @@ Module_2 = function(x,dict,total_prob){
   df = df %>% filter(!is.null(virus)) %>% mutate(pVal_f = mapply(bt, df$filtered, df$all_f,df$V1)) %>% arrange(pVal_f)
   return(df)
 }
-
-
